24590267524223aaca82e7a0a8cd447e58bd26a1
Leda Yang <yangleda@outlook.com>
2025-03-19 16:19:34 +0800

    Update MWITools.js

--- MWITools.js
+++ MWITools.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MWITools
 // @namespace    http://tampermonkey.net/
-// @version      20.7
+// @version      20.8
 // @description  Tools for MilkyWayIdle. Shows total action time. Shows market prices. Shows action number quick inputs. Shows how many actions are needed to reach certain skill level. Shows skill exp percentages. Shows total networth. Shows combat summary. Shows combat maps index. Shows item level on item icons. Shows how many ability books are needed to reach certain level. Shows market equipment filters.
 // @author       bot7420
 // @license      CC-BY-NC-SA-4.0
